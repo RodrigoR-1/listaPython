14f13bf12d6c46b0a7d729f3fb7094dfd96716d8
RodrigoR-1 <rodjunior875@gmail.com>
2026-05-13 14:20:05 +0000

    V.1

--- teste.py
+++ teste.py
@@ -1,1 +1,21 @@
-
+# Criando um Teste com Pontuação--
+print("Teste seus Conhecimentos sobre Ciências")
+pontos =0
+#--Questão 1--
+print("qual é a formula da água?")
+print("a-H2O \n b-CO2 \n c-AL")
+resposta1 = input("digite a resposta: ").lower()
+if resposta1 == "a":
+    print("resposta correta")
+    pontos = pontos +1
+else:
+    print("você errou")
+#Questão 2--
+print("2- 0 sol é : ")
+print("a-Satélite \n b-Estrela \n c-Asteróide")
+resposta2 = input("digite a Resposta: ").lower()
+if resposta2 == "b":
+    print("Você acertou")
+    pontos = pontos +1
+else:
+    print("Você errou")
